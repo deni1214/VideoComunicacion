9ef7a430754de8eb4cb4842ed756b43f3d7e2370
Denilson <Denilson@DESKTOP-1D8LAU1>
2021-05-18 19:59:16 -0500

    dfg

--- src/videocomunicacion/VideoComunicacion.java
+++ src/videocomunicacion/VideoComunicacion.java
@@ -10,12 +10,29 @@ package videocomunicacion;
  * @author Denilson
  */
 public class VideoComunicacion {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    private static String nombre;
+    private static int CantidadDeUsuarios;
+    private static int Tiempo;
+    private static String nivel;
+    private static int Estrellas;
+            
+    public VideoComunicacion(String nombre, int CantidadDeUsuarios, int Tiempo, String nivel, int Estrellas){
+        VideoComunicacion.nombre = nombre;
+        VideoComunicacion.CantidadDeUsuarios = CantidadDeUsuarios;
+        VideoComunicacion.Estrellas = Estrellas;
+        VideoComunicacion.Tiempo = Tiempo;
+        VideoComunicacion.nivel = nivel;
+        
+    }
+    
+    public static int cali(){
+        return CantidadDeUsuarios*Tiempo*Estrellas;
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        VideoComunicacion MiExamen = new VideoComunicacion ("Meet", 40, 2, "libre", 5);
+        System.out.println(cali());
     }
     
 }
